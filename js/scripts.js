67d89260fac1ad11d524dff43f0a0048bcad1146
Pawel Morka <morka.paul@gmail.com>
2018-01-29 12:54:47 +0100

    Add index.html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var a = prompt('Wstaw wartość a:');
+	b = prompt('Wstaw wartość b:');
+	value = (a * a) - (2 * a * b) + (b * b);
+console.log('Wynik to: ' + value)
+
+if (value > 0) {
+    alert('Wynik dodatni');
+} else if (value < 0) {
+    alert('Wynik ujemny');
+} else {
+    alert('Wynik równy zero!');
+}
