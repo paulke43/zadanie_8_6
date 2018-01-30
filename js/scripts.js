93827c906749f358f21553ddab16fd7d29e7981b
Pawel Morka <morka.paul@gmail.com>
2018-01-30 10:16:27 +0100

    Changes scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-var a = prompt('Wstaw wartość a:');
-	b = prompt('Wstaw wartość b:');
-	value = (a * a) - (2 * a * b) + (b * b);
+var a;
+var b;
+
+a = prompt('Wstaw wartość a:');
+b = prompt('Wstaw wartość b:');
+	
+var value = (a * a) - (2 * a * b) + (b * b);
+
 console.log('Wynik to: ' + value)
 
 if (value > 0) {
     alert('Wynik dodatni');
-} else if (value < 0) {
-    alert('Wynik ujemny');
-} else {
+} else if (value <= 0) {
     alert('Wynik równy zero!');
+} else {
+    alert('Wstaw odpowiednie wartości');
 }
